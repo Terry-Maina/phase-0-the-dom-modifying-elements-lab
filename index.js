289838d4cf9f18ec791b3865d48cbefb1789b40f
Teresa <tenjerim@gmail.com>
2022-12-04 12:41:08 +0300

    done

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ main.remove();
 let newHeader = document.createElement("h1");
 newHeader.setAttribute("id","victory");
 document.body.appendChild(newHeader);
-newHeader.textContent = `<h1>"ivian is the champion"</h1>`
+newHeader.textContent = `<h1>"Teresa is the champion"</h1>`
