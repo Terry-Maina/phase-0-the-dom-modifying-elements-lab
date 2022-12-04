9de7b1073133ca64058bfb2da1870bbcb145ac3b
Teresa <tenjerim@gmail.com>
2022-12-04 12:40:09 +0300

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code here!
-const main = document.querySelector('main#main')
-main.remove()
-let newHeader = document.createElement("h1")
-//setAttribute() method is used to set or add an attribute to a particular element and provides a value to it
-newHeader.setAttribute("id","victory")
-newHeader.innerHTML = `<h1>Teresa is the champion</h1>`
+let main = document.getElementById("main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+document.body.appendChild(newHeader);
+newHeader.textContent = `<h1>"ivian is the champion"</h1>`
